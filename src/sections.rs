87634f90a647e6f797f62faba3f92c30cc0b11ad
david <davidcalieln@gmail.com>
2023-04-27 16:38:57 -0700

    Add known section IDs

--- src/sections.rs
+++ src/sections.rs
@@ -17,6 +17,53 @@ pub enum SectionKind {
     Custom(std::borrow::Cow<'static, str>),
 }
 
+macro_rules! known_ids {
+    ($(
+        $(#[$meta:meta])*
+        $name:ident = $value:literal;
+    )*) => {
+        impl SectionKind {
+            $(
+                $(#[$meta])*
+                pub const $name: Self = Self::Id({
+                    // Safety: value should not be zero
+                    unsafe {
+                        std::num::NonZeroU8::new_unchecked($value)
+                    }
+                });
+            )*
+        }
+    };
+}
+
+// Id should not be 0
+known_ids! {
+    /// [The *type* section](https://webassembly.github.io/spec/core/binary/modules.html#binary-typesec).
+    TYPE = 1;
+    /// [The *import* section](https://webassembly.github.io/spec/core/binary/modules.html#binary-importsec).
+    IMPORT = 2;
+    /// [The *function* section](https://webassembly.github.io/spec/core/binary/modules.html#binary-funcsec).
+    FUNC = 3;
+    /// [The *table* section](https://webassembly.github.io/spec/core/binary/modules.html#binary-tablesec)
+    TABLE = 4;
+    /// [The *memory* section](https://webassembly.github.io/spec/core/binary/modules.html#binary-memsec)
+    MEMORY = 5;
+    /// [The *global* section](https://webassembly.github.io/spec/core/binary/modules.html#binary-globalsec)
+    GLOBAL = 6;
+    /// [The *export* section](https://webassembly.github.io/spec/core/binary/modules.html#binary-exportsec)
+    EXPORT = 7;
+    /// [The *start* section](https://webassembly.github.io/spec/core/binary/modules.html#binary-startsec)
+    START = 8;
+    /// [The *element* section](https://webassembly.github.io/spec/core/binary/modules.html#binary-elemsec)
+    ELEMENT = 9;
+    /// [The *code* section](https://webassembly.github.io/spec/core/binary/modules.html#binary-codesec)
+    CODE = 10;
+    /// [The *data* section](https://webassembly.github.io/spec/core/binary/modules.html#binary-datasec)
+    DATA = 11;
+    /// [The *data count* section](https://webassembly.github.io/spec/core/binary/modules.html#binary-datacountsec)
+    DATA_COUNT = 12;
+}
+
 /// Represents a
 /// [WebAssembly section](https://webassembly.github.io/spec/core/binary/modules.html#sections).
 #[derive(Debug)]
