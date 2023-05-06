7195aa6278c5397f313a0b02fcebf9f8ea0abbb2
david <davidcalieln@gmail.com>
2023-05-05 18:49:19 -0700

    Add more tests for leb128_s32

--- tests/leb128.rs
+++ tests/leb128.rs
@@ -33,11 +33,26 @@ fn examples_s32() {
     }
 
     assert_eq_decoded!(0, &[0]);
-    //assert_eq_decoded!(-1, &[0]);
+    assert_eq_decoded!(-1, &[0x7F]);
     assert_eq_decoded!(63, &[0x3F]);
     assert_eq_decoded!(-64, &[0x40]);
     assert_eq_decoded!(-2, &[0x7E]);
     assert_eq_decoded!(64, &[0xC0, 0]);
-    assert_eq_decoded!(-2, &[0xFE, 0x7F]);
     assert_eq_decoded!(128, &[0x80, 1]);
+    assert_eq_decoded!(i32::from(u8::MAX), &[0xFF, 1]);
+    assert_eq_decoded!(i32::from(i8::MIN), &[0x80, 0x7F]);
+    assert_eq_decoded!(i32::from(i8::MAX), &[0xFF, 0]);
+    assert_eq_decoded!(-2, &[0xFE, 0x7F]);
+    assert_eq_decoded!(8191, &[0xFF, 0x3F]);
+    assert_eq_decoded!(-8192, &[0x80, 0x40]);
+    assert_eq_decoded!(-2, &[0xFE, 0xFF, 0x7F]);
+    assert_eq_decoded!(1048575, &[0xFF, 0xFF, 0x3F]);
+    assert_eq_decoded!(-1048576, &[0x80, 0x80, 0x40]);
+    assert_eq_decoded!(i32::from(i16::MAX), &[0xFF, 0xFF, 1]);
+    assert_eq_decoded!(i32::from(i16::MIN), &[0x80, 0x80, 0x7E]);
+    assert_eq_decoded!(i32::from(u16::MAX), &[0xFF, 0xFF, 0x03]);
+    assert_eq_decoded!(134217727, &[0xFF, 0xFF, 0xFF, 0x3F]);
+    assert_eq_decoded!(-134217728, &[0x80, 0x80, 0x80, 0x40]);
+    assert_eq_decoded!(i32::MAX, &[0xFF, 0xFF, 0xFF, 0xFF, 0x07]);
+    assert_eq_decoded!(i32::MIN, &[0x80, 0x80, 0x80, 0x80, 0x78]);
 }
