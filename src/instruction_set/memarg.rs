c1d7fa30221cc838a05ffab46209e2fc0cfb0d53
david <davidcalieln@gmail.com>
2023-05-15 18:01:14 -0700

    Add Instruction::requires_multi_memory

--- src/instruction_set/memarg.rs
+++ src/instruction_set/memarg.rs
@@ -74,11 +74,76 @@ impl MemArg {
         self.memory
     }
 
-    /// Returns `true` if the `memarg` references a memory other than the default memory `0`.
+    /// Returns `true` if the `memarg` references a memory other than the default memory (index
+    /// `0`).
     ///
     /// See the [multiple memory proposal](https://github.com/WebAssembly/multi-memory) for more
     /// information.
     pub const fn requires_multi_memory(&self) -> bool {
-        self.memory.to_u32() == 0
+        self.memory.to_u32() != 0
+    }
+}
+
+impl<B: crate::bytes::Bytes> crate::instruction_set::Instruction<'_, B> {
+    /// Returns `true` if the instruction references memory other than the default memory (index
+    /// `0`), which requires the
+    /// [multiple memory proposal](https://github.com/WebAssembly/multi-memory).
+    pub const fn requires_multi_memory(&self) -> bool {
+        match self {
+            Self::I32Load(memarg)
+            | Self::I64Load(memarg)
+            | Self::F32Load(memarg)
+            | Self::F64Load(memarg)
+            | Self::I32Load8S(memarg)
+            | Self::I32Load8U(memarg)
+            | Self::I32Load16S(memarg)
+            | Self::I32Load16U(memarg)
+            | Self::I64Load8S(memarg)
+            | Self::I64Load8U(memarg)
+            | Self::I64Load16S(memarg)
+            | Self::I64Load16U(memarg)
+            | Self::I64Load32S(memarg)
+            | Self::I64Load32U(memarg)
+            | Self::I32Store(memarg)
+            | Self::I64Store(memarg)
+            | Self::F32Store(memarg)
+            | Self::F64Store(memarg)
+            | Self::I32Store8(memarg)
+            | Self::I32Store16(memarg)
+            | Self::I64Store8(memarg)
+            | Self::I64Store16(memarg)
+            | Self::I64Store32(memarg)
+            | Self::V128Load(memarg)
+            | Self::V128Load8x8S(memarg)
+            | Self::V128Load8x8U(memarg)
+            | Self::V128Load16x4S(memarg)
+            | Self::V128Load16x4U(memarg)
+            | Self::V128Load32x2S(memarg)
+            | Self::V128Load32x2U(memarg)
+            | Self::V128Load8Splat(memarg)
+            | Self::V128Load16Splat(memarg)
+            | Self::V128Load32Splat(memarg)
+            | Self::V128Load64Splat(memarg)
+            | Self::V128Load32Zero(memarg)
+            | Self::V128Load64Zero(memarg)
+            | Self::V128Store(memarg)
+            | Self::V128Load8Lane(memarg, _)
+            | Self::V128Load16Lane(memarg, _)
+            | Self::V128Load32Lane(memarg, _)
+            | Self::V128Load64Lane(memarg, _)
+            | Self::V128Store8Lane(memarg, _)
+            | Self::V128Store16Lane(memarg, _)
+            | Self::V128Store32Lane(memarg, _)
+            | Self::V128Store64Lane(memarg, _) => memarg.requires_multi_memory(),
+            Self::MemorySize(index)
+            | Self::MemoryGrow(index)
+            | Self::MemoryInit(_, index)
+            | Self::MemoryFill(index) => index.to_u32() != 0,
+            Self::MemoryCopy {
+                destination,
+                source,
+            } => destination.to_u32() != 0 || source.to_u32() != 0,
+            _ => false,
+        }
     }
 }
