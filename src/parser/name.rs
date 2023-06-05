98971d73c15d1e863b386f177a7f888def91b3a4
david <davidcalieln@gmail.com>
2023-06-05 15:35:26 -0700

    Add initial name::Chars implementation

--- src/parser/name.rs
+++ src/parser/name.rs
@@ -5,20 +5,23 @@ use crate::{
     bytes::{self, Bytes},
     parser::{self, ResultExt as _},
 };
-use core::fmt::{Debug, Display, Formatter, Write};
+use core::{
+    fmt::{Debug, Display, Formatter, Write},
+    num::NonZeroU8,
+};
 
 /// Describes an invalid byte sequence or code point that was encountered while decoding a
 /// [`Name`].
 #[derive(Clone, Copy)]
 pub struct InvalidCodePoint {
-    length: u8,
+    length: NonZeroU8,
     bytes: [u8; 4],
 }
 
 impl InvalidCodePoint {
     fn bytes(&self) -> Option<&[u8]> {
-        let length = usize::from(self.length);
-        if (1..=self.bytes.len()).contains(&length) {
+        let length = usize::from(self.length.get());
+        if length <= self.bytes.len() {
             Some(&self.bytes[0..length])
         } else {
             None
@@ -89,6 +92,8 @@ struct CharsBuffer {
     /// - Lower 4 bits contain number of saved bytes in `buffer`.
     /// - Upper 4 bits contain number of valid UTF-8 bytes in `buffer`.
     lengths: u8,
+    /// Length of the error after `valid_len`.
+    bad_sequence: Option<NonZeroU8>,
     /// # Safety
     ///
     /// The first `valid_len` bytes **must** be valid UTF-8.
@@ -96,50 +101,116 @@ struct CharsBuffer {
 }
 
 impl CharsBuffer {
+    #[inline]
     fn saved_len(&self) -> u8 {
         self.lengths & 0xF
     }
 
+    #[inline]
     fn valid_len(&self) -> u8 {
         self.lengths >> 4
     }
 
     fn valid(&self) -> &str {
         let valid_bytes = &self.buffer[0..usize::from(self.valid_len())];
-
-        cfg_if::cfg_if! {
-            if #[cfg(debug_assertions)] {
-                core::str::from_utf8(valid_bytes).unwrap()
-            } else {
-                // Safety: it is an invariant that valid_len bytes are valid
-                unsafe {
-                    core::str::from_utf8_unchecked(valid_bytes)
-                }
-            }
+        if cfg!(debug_assertions) {
+            core::str::from_utf8(valid_bytes).unwrap()
+        } else {
+            // Safety: it is an invariant that valid_len bytes are valid
+            unsafe { core::str::from_utf8_unchecked(valid_bytes) }
         }
     }
 
     fn advance(&mut self, amount: u8) {
         debug_assert!(amount <= self.saved_len());
-        debug_assert!(amount <= self.valid_len());
 
+        self.lengths |= (self.saved_len() - amount) & 0xF;
 
+        if self.valid_len() > 0 {
+            debug_assert!(amount <= self.valid_len());
+            self.lengths |= (self.valid_len() - amount) << 4;
+        }
+
+        let start = usize::from(amount);
+        let new_length = start + usize::from(self.saved_len());
+        self.buffer.copy_within(start..new_length, 0);
     }
 
-    fn take_char(&mut self) -> Option<char> {
+    fn take_char(&mut self) -> Result<Option<char>, NameError> {
         if self.valid_len() > 0 {
             let mut chars = self.valid().chars();
 
-            // Safety: check that string is not empty occurs above
-            let c = unsafe { chars.next().unwrap_unchecked() };
+            let c = if cfg!(debug_assertions) {
+                chars.next().unwrap()
+            } else {
+                // Safety: check above ensures string is not empty
+                unsafe { chars.next().unwrap_unchecked() }
+            };
 
             // Skip the amount of bytes that were read?
             self.advance(chars.as_str().len() as u8);
 
-            Some(c)
+            Ok(Some(c))
+        } else if let Some(bad) = self.bad_sequence.take() {
+            let bad_len = usize::from(bad.get());
+            let mut bytes = [0u8; 4];
+            bytes[0..bad_len].copy_from_slice(&self.buffer[0..bad_len]);
+
+            self.advance(bad.get());
+
+            Err(NameError::BadBytes(InvalidCodePoint { length: bad, bytes }))
         } else {
-            None
+            Ok(None)
         }
+    }
+
+    fn fill(
+        &mut self,
+        offset: &mut u64,
+        length: &mut u32,
+        bytes: &impl Bytes,
+    ) -> Result<(), NameError> {
+        // take_char would return Ok(Some) or Err for the same conditions here
+        if self.valid_len() > 0 || self.bad_sequence.is_some() {
+            return Ok(());
+        }
+
+        let saved_length = usize::from(self.saved_len());
+        if saved_length < self.buffer.len() {
+            let result = bytes
+                .read(offset, &mut self.buffer[saved_length..])
+                .map(|buf| buf.len() as u8);
+
+            match result {
+                Ok(filled) => {
+                    self.lengths |= (saved_length as u8 + filled) & 0xF;
+                    *length -= u32::from(filled);
+                }
+                Err(e) => {
+                    *length = 0;
+                    self.lengths = 0;
+                    self.bad_sequence = None;
+                    return Err(NameError::BadInput(e));
+                }
+            }
+        }
+
+        debug_assert_eq!(self.valid_len(), 0);
+
+        let valid_len = match core::str::from_utf8(&self.buffer[0..usize::from(self.saved_len())]) {
+            Ok(s) => s.len(),
+            Err(e) => {
+                if let Some(bad_len) = e.error_len() {
+                    self.bad_sequence =
+                        Some(u8::try_from(bad_len).ok().and_then(NonZeroU8::new).unwrap());
+                }
+
+                e.valid_up_to()
+            }
+        };
+
+        self.lengths |= (valid_len as u8) << 4;
+        Ok(())
     }
 }
 
@@ -173,36 +244,13 @@ impl<B: Bytes> Chars<B> {
             return Ok(None);
         }
 
-        if let c @ Some(_) = self.buffer.take_char() {
-            return Ok(c);
-        }
+        self.buffer.fill(
+            &mut self.name.offset,
+            &mut self.name.length,
+            &self.name.bytes,
+        )?;
 
-        // TODO: If buffer len > valid len then there are potential invalid bytes?
-
-        // if usize::from(self.buffer.saved_len()) < self.buffer.len() {
-        //     let result = self
-        //         .name
-        //         .bytes
-        //         .read(
-        //             &mut self.name.offset,
-        //             &mut self.buffer[usize::from(self.saved)..],
-        //         )
-        //         .map(|buffer| buffer.len() as u8);
-
-        //     match result {
-        //         Ok(advanced) => {
-        //             self.name.length += u32::from(advanced);
-        //             self.saved += advanced;
-        //         }
-        //         Err(e) => {
-        //             self.name.length = 0;
-        //             self.saved = 0;
-        //             return Err(NameError::BadInput(e));
-        //         }
-        //     }
-        // }
-
-        todo!()
+        self.buffer.take_char()
     }
 }
 
@@ -218,6 +266,8 @@ impl<B: Bytes> Iterator for Chars<B> {
         (core::cmp::min(1, max), Some(max))
     }
 }
+
+impl<B: Bytes> core::iter::FusedIterator for Chars<B> {}
 
 /// An iterator over the [`char`]s of a [`Name`] that substitutes invalid byte sequences and other errors with [`char::REPLACEMENT_CHARACTER`].
 ///
@@ -284,6 +334,8 @@ impl<B: Bytes> Iterator for CharsLossy<B> {
         self.inner.size_hint()
     }
 }
+
+impl<B: Bytes> core::iter::FusedIterator for CharsLossy<B> {}
 
 /// A UTF-8 string [name](https://webassembly.github.io/spec/core/binary/values.html#names).
 #[derive(Clone, Copy)]
