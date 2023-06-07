6c6484e25c4c60ab98a632761be7c728a89d08f4
david <davidcalieln@gmail.com>
2023-06-06 22:11:52 -0700

    Add bytes macro to sections tests

--- tests/sections.rs
+++ tests/sections.rs
@@ -1,5 +1,15 @@
 use wasmiter::component;
 
+macro_rules! bytes {
+    ($($e:expr,)*) => {{
+        let mut bytes = std::vec::Vec::<u8>::new();
+        $(
+            bytes.extend_from_slice(AsRef::<[u8]>::as_ref(&$e));
+        )*
+        bytes
+    }};
+}
+
 #[test]
 fn type_section() {
     let bytes = [
@@ -16,57 +26,63 @@ fn type_section() {
 
 #[test]
 fn export_section() {
-    let bytes = [
-        1u8, // count
-        0xC, // name length
-        0x6D, 0x79, 0x45, 0x78, 0x70, 0x6f, 0x72, 0x74, 0x4E, 0x61, 0x6D, 0x65,
-        0, // export func
-        0, // funcidx
-    ];
+    let bytes = bytes! {
+        [
+            1, // count
+            0xC, // name length
+        ],
+        b"myExportName",
+        [
+            0, // export func
+            0, // funcidx
+        ],
+    };
 
     insta::assert_display_snapshot!(component::ExportsComponent::new(0, bytes.as_slice()).unwrap());
 }
 
 #[test]
 fn import_section() {
-    let bytes = [
-        4u8, // count
-
-        // Import 0
-        3, // name length
-        0x65, 0x6E, 0x76,
-        0xB, // name length
-        0x64, 0x6F, 0x53, 0x6F, 0x6D, 0x65, 0x53, 0x74, 0x75, 0x66, 0x66,
-        0, // import func
-        0, // typeidx
-
-        // Import 1
-        3, // name length
-        0x65, 0x6E, 0x76,
-        6, // name length
-        0x6D, 0x65, 0x6D, 0x6F, 0x72, 0x79,
-        2, // import memory
-        0, // limit w/o maximum
-        0x10, // limit minimum
-
-        // Import 2
-        2, // name length
-        0x72, 0x74,
-        0xA, // name length
-        0x72, 0x65, 0x66, 0x65, 0x72, 0x65, 0x6E, 0x63, 0x65, 0x73,
-        1, // import table,
-        0x6F, // externref
-        0, 0, // limits
-
-        // Import 3
-        2, // name length
-        0x72, 0x74,
-        8, // name length
-        0x73, 0x74, 0x61, 0x63, 0x6B, 0x70, 0x74, 0x72,
-        3, // import global
-        0x7F, // i32
-        1, // mutable
-    ];
+    let bytes = bytes! {
+        [
+            4, // count
+            3, // module name length
+        ],
+        b"env",
+        [0xB], // name length
+        b"doSomeStuff",
+        [
+            0, // import func
+            0, // typeidx
+            3, // module name length
+        ],
+        b"env",
+        [6], // name length
+        b"memory",
+        [
+            2, // import memory
+            0, // limit w/o maximum
+            0x10, // limit minimum
+            2, // module name length
+        ],
+        b"rt",
+        [0xA], // name length
+        b"references",
+        [
+            1, // import table,
+            0x6F, // externref
+            0, 0, // limits
+            2, // module name length
+        ],
+        b"rt",
+        [8], // name length
+        b"stackptr",
+        [
+            3, // import global
+            0x7F, // i32
+            1, // mutable
+        ],
+    };
 
     insta::assert_display_snapshot!(component::ImportsComponent::new(0, bytes.as_slice()).unwrap());
 }
