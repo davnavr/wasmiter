809f043e7725b4d99d4020e04f5cac38c5d5ecae
david <davidcalieln@gmail.com>
2023-06-09 00:08:00 -0700

    Update tests to use new display structs

--- tests/modules.rs
+++ tests/modules.rs
@@ -7,17 +7,17 @@ fn basic_module() {
         i32.add))
 "#;
     let wasm = wat::parse_str(wat).unwrap();
-    insta::assert_display_snapshot!(&wasmiter::parse_module_sections(wasm.as_slice()).unwrap());
+    insta::assert_display_snapshot!(wasmiter::parse_module_sections(wasm.as_slice()).unwrap().display_module());
 }
 
 #[test]
 fn exception_handling() {
     let wasm = wat::parse_str(include_str!("modules/exception_handling.wat")).unwrap();
-    insta::assert_display_snapshot!(&wasmiter::parse_module_sections(wasm.as_slice()).unwrap());
+    insta::assert_display_snapshot!(wasmiter::parse_module_sections(wasm.as_slice()).unwrap().display_module());
 }
 
 #[test]
 fn all_the_things() {
     let wasm = wat::parse_str(include_str!("modules/all_the_things.wat")).unwrap();
-    insta::assert_display_snapshot!(&wasmiter::parse_module_sections(wasm.as_slice()).unwrap());
+    insta::assert_display_snapshot!(wasmiter::parse_module_sections(wasm.as_slice()).unwrap().display_module());
 }
