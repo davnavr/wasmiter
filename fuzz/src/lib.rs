24de1d0f0e26a739836a7cef7c97c58ee403f787
david <davidcalieln@gmail.com>
2023-06-10 22:09:11 -0700

    Move process_sections fuzz helper

--- fuzz/src/lib.rs
+++ fuzz/src/lib.rs
@@ -1,52 +1,151 @@
-use libfuzzer_sys::arbitrary::{self, Arbitrary};
+//! Helper structs and functions for fuzzing targets.
 
-#[derive(Arbitrary, Clone, Copy, Debug, Default)]
-pub struct WasmiterConfig;
+use wasmiter::{component::KnownSection, custom::KnownCustomSection};
 
-impl wasm_smith::Config for WasmiterConfig {
-    fn bulk_memory_enabled(&self) -> bool {
-        true
+mod config;
+
+pub use config::{ConfiguredModule, WasmiterConfig};
+
+pub fn process_sections(wasm: &[u8]) -> wasmiter::parser::Result<()> {
+    for result in wasmiter::parse_module_sections(wasm).unwrap() {
+        match KnownSection::interpret(result?) {
+            Ok(known) => match known? {
+                KnownSection::Type(mut types) => loop {
+                    let result = types.parse(
+                        |params| {
+                            for item in params {
+                                let _ = item?;
+                            }
+                            Ok(())
+                        },
+                        |(), results| {
+                            for item in results {
+                                let _ = item?;
+                            }
+                            Ok(())
+                        },
+                    )?;
+
+                    if result.is_none() {
+                        break;
+                    }
+                },
+                KnownSection::Import(imports) => {
+                    for result in imports {
+                        let entry = result?;
+                        let _ = entry.module().try_into_string()?;
+                        let _ = entry.name().try_into_string()?;
+                    }
+                }
+                KnownSection::Function(functions) => {
+                    for result in functions {
+                        let _ = result?;
+                    }
+                }
+                KnownSection::Table(tables) => {
+                    for result in tables {
+                        let _ = result?;
+                    }
+                }
+                KnownSection::Memory(mems) => {
+                    for result in mems {
+                        let _ = result?;
+                    }
+                }
+                KnownSection::Global(mut globals) => loop {
+                    if globals.parse(|_, _| Ok(()))?.is_none() {
+                        break;
+                    }
+                },
+                KnownSection::Export(exports) => {
+                    for result in exports {
+                        let _ = result?.name().try_into_string()?;
+                    }
+                }
+                KnownSection::Start(_) => (),
+                KnownSection::Element(mut elements) => loop {
+                    if elements.parse(|_| Ok(()), |(), _| Ok(()))?.is_none() {
+                        break;
+                    }
+                },
+                KnownSection::Code(code) => {
+                    for result in code {
+                        let entry = result?;
+                        entry.read(|_| wasmiter::parser::Result::Ok(()), |(), _| Ok(()))?;
+                    }
+                }
+                KnownSection::Data(mut data) => loop {
+                    if data.parse(|_| Ok(()), |(), _| Ok(()))?.is_none() {
+                        break;
+                    }
+                },
+                KnownSection::DataCount(_) => (),
+                KnownSection::Tag(tag) => {
+                    for result in tag {
+                        let _ = result?;
+                    }
+                }
+                bad => panic!("unsupported section {:?}", bad.id()),
+            },
+            Err(possibly_custom) => {
+                if let Ok(result) =
+                    wasmiter::custom::CustomSection::try_from_section(possibly_custom)
+                {
+                    if let Ok(known) = KnownCustomSection::interpret(result?) {
+                        match known {
+                            KnownCustomSection::Name(name) => {
+                                for result in name {
+                                    use wasmiter::custom::name::NameSubsection;
+
+                                    if let Ok(result) = result {
+                                        match result? {
+                                            NameSubsection::ModuleName(name) => {
+                                                let _ = name.try_into_string()?;
+                                            }
+                                            NameSubsection::FunctionName(name_map) => {
+                                                for result in name_map {
+                                                    let name_assoc = result?;
+                                                    let _ = name_assoc.name().try_into_string()?;
+                                                }
+                                            }
+                                            NameSubsection::LocalName(mut indirect_name_map) => {
+                                                loop {
+                                                    let result = indirect_name_map.parse(
+                                                        |_, name_map| {
+                                                            for result in name_map {
+                                                                let name_assoc = result?;
+                                                                let _ = name_assoc
+                                                                    .name()
+                                                                    .try_into_string()?;
+                                                            }
+                                                            Ok(())
+                                                        },
+                                                    )?;
+
+                                                    if result.is_some() {
+                                                        break;
+                                                    }
+                                                }
+                                            }
+                                            NameSubsection::TagName(name_map) => {
+                                                for result in name_map {
+                                                    let name_assoc = result?;
+                                                    let _ = name_assoc.name().try_into_string()?;
+                                                }
+                                            }
+                                            bad => {
+                                                panic!("unsupported name subsection {:?}", bad.id())
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            bad => panic!("unsupported custom section {:?}", bad.name()),
+                        }
+                    }
+                }
+            }
+        }
     }
-
-    fn reference_types_enabled(&self) -> bool {
-        true
-    }
-
-    fn tail_call_enabled(&self) -> bool {
-        true
-    }
-
-    fn simd_enabled(&self) -> bool {
-        true
-    }
-
-    fn exceptions_enabled(&self) -> bool {
-        true
-    }
-
-    fn multi_value_enabled(&self) -> bool {
-        true
-    }
-
-    fn saturating_float_to_int_enabled(&self) -> bool {
-        true
-    }
-
-    fn sign_extension_ops_enabled(&self) -> bool {
-        true
-    }
-
-    fn memory64_enabled(&self) -> bool {
-        true
-    }
-
-    fn generate_custom_sections(&self) -> bool {
-        true
-    }
-
-    fn threads_enabled(&self) -> bool {
-        true
-    }
+    Ok(())
 }
-
-pub type ConfiguredModule = wasm_smith::ConfiguredModule<WasmiterConfig>;
